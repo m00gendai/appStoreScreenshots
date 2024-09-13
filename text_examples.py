095e42c565efd1969e5b8f4edd51e37b5131b654
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2024-09-13 21:36:53 +0200

    Create text_examples.py

---
+++ text_examples.py
@@ -0,0 +1,60 @@
+# Examples are from Arsenal Gun Collection App
+
+texts = [
+    {
+        "img1": {
+            "de": "Die eigene Waffensammlung bequem überall verwalten",
+            "en": "Conveniently manage your own gun collection anywhere",
+            "fr": "Gérer facilement sa collection d'armes partout",
+            "it": "Gestire comodamente e ovunque la propria collezione di armi",
+        }
+    },
+    {
+        "img2": {
+            "de": "Import/Export von Datenbanken, einschliesslich benutzerdefinierter CSV-Dateien",
+            "en": "Import/export of databases, including custom CSV files",
+            "fr": "Import/export de bases de données, y compris des fichiers CSV personnalisés",
+            "it": "Importazione/esportazione di database, inclusi file CSV personalizzati",
+        }
+    },
+    {
+        "img3": {
+            "de": "Verschiedene Farbprofile und fünf Sprachen (einschliesslich Rumantsch!)",
+            "en": "Various color profiles and five languages (including Rumantsch!)",
+            "fr": "Divers profils de couleurs et cinq langues (y compris le rumantsch !)",
+            "it": "Vari profili di colore e cinque lingue (compreso il rumantsch!)",
+        }
+    },
+    {
+        "img4": {
+            "de": "Verschiedene PDF-Ausdrucke, z. B. für die Liste gemäss Artikel 5",
+            "en": "Various PDF printouts, e.g., for the list in accordance with Article 5",
+            "fr": "Différents documents PDF, par exemple pour la liste conformément à l'article 5",
+            "it": "Diversi documenti PDF, ad es. per l'elenco in conformità con l'articolo 5",
+        }
+    },
+    {
+        "img5": {
+            "de": "Anzahl der Schüsse und Munitionsvorrat sind verknüpft",
+            "en": "Number of shots and ammunition stock are linked",
+            "fr": "Le nombre de tirs et le stock de munitions sont liés",
+            "it": "Numero di colpi e scorta di munizioni sono collegati",
+        }
+    },
+    {
+        "img6": {
+            "de": "Munitionsverwaltung und Lagerkontrolle leicht gemacht",
+            "en": "Ammunition management and inventory control made easy",
+            "fr": "Gestion des munitions et contrôle des stocks simplifiés",
+            "it": "Gestione delle munizioni e controllo dell'inventario semplificati",
+        }
+    },
+    {
+        "img7": {
+            "de": "Viele Eingabefelder mit hilfreichen Assistenten",
+            "en": "Many input fields with helpful wizards",
+            "fr": "De nombreux champs de saisie avec des assistants utiles",
+            "it": "Molti campi di input con utili assistenti",
+        }
+    },
+]
